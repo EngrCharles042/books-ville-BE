664eedc00189ed98f38ee58cf353ea85f082c82e
Emmaibe <emmanuelibenwankwo@gmail.com>
2024-01-16 11:41:37 +0100

    transaction controller update

--- BooksVille/src/main/java/BooksVille/payload/request/TransactionRequest.java
+++ BooksVille/src/main/java/BooksVille/payload/request/TransactionRequest.java
@@ -1,6 +1,8 @@
 package BooksVille.payload.request;
 
 import BooksVille.entities.enums.TransactStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,11 +13,15 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionRequest {
+    @NotNull(message = "Amount cannot be null!")
     private BigDecimal amount;
 
+    @NotNull(message = "Transaction Status cannot be null!")
     private TransactStatus status;
 
+    @NotBlank(message = "Reference Id cannot be blank!")
     private String referenceId;
 
+    @NotNull(message = "Book Id cannot be null!")
     private Long bookEntityId;
 }
