6a576d3aa43c34fb21d7bb1a32db998413d6d601
Praise Udoh <praiseubong28@gmail.com>
2024-02-03 15:58:30 +0100

    Downloadbook

--- BooksVille/src/main/java/BooksVille/repositories/TransactionEntityRepository.java
+++ BooksVille/src/main/java/BooksVille/repositories/TransactionEntityRepository.java
@@ -1,7 +1,13 @@
 package BooksVille.repositories;
 
+import BooksVille.entities.model.BookEntity;
 import BooksVille.entities.model.TransactionEntity;
+import BooksVille.entities.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TransactionEntityRepository extends JpaRepository<TransactionEntity, Long> {
+    Optional<TransactionEntity> findByUserEntityAndBookEntity(UserEntity userEntity, BookEntity bookEntity);
+
 }
