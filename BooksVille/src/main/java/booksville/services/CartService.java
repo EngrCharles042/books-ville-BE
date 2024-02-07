607aa156dfb885df000b8581b7d973bc7ef5c44d
Emmaibe <emmanuelibenwankwo@gmail.com>
2024-02-07 23:41:45 +0100

    cart and book controller correction and update

--- BooksVille/src/main/java/booksville/services/CartService.java
+++ BooksVille/src/main/java/booksville/services/CartService.java
@@ -1,9 +1,13 @@
 package booksville.services;
 
 import booksville.payload.response.ApiResponse;
+import booksville.payload.response.BookEntityResponse;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface CartService {
     ResponseEntity<ApiResponse<String>> addToCart(Long bookId);
     ResponseEntity<ApiResponse<String>> removeFromCart(Long bookId);
+    ResponseEntity<ApiResponse<List<BookEntityResponse>>> getCart();
 }
